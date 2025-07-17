0dd95e1c1face567463240f1c0fae5432dcf0422
Usuari IT <Usuari IT@LAPTOP-IT>
2025-07-17 11:22:25 +0200

    Deploy website - based on 01fd4b2a8ee55ddf625471833252d19fa7c9bb29

--- clients/puignau/CRM/integrity_check/view_preped_lines_without_pedido.sql
+++ clients/puignau/CRM/integrity_check/view_preped_lines_without_pedido.sql
@@ -2,7 +2,7 @@ select *
 from Pers_PrePedido_Lineas ppl
 inner join Pers_PrePedido pp on pp.IdPrePedido = ppl.IdPrePedido
 where 
-	(ppl.IdPedido is null 
-		or ppl.IdPedidoLinea = 0)
-	and idestado = 2
-	and Fecha_Entrega > GETDATE()
+    (ppl.IdPedido is null 
+        or ppl.IdPedidoLinea = 0)
+    and idestado = 2
+    and Fecha_Entrega > GETDATE()
