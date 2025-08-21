06e9da5dd3350b53a567f8999c604bbddc35b8da
estmi-nowtech <esteve.miquel@nowtechsite.com>
2025-08-21 15:46:40 +0200

    Deploy website - based on 4d9d930142d910c9314d768d7e5f36891d778dde

--- clients/puignau-bcn/4208/fpers_reserves_producte_pa_pugi_per_puba.sql
+++ clients/puignau-bcn/4208/fpers_reserves_producte_pa_pugi_per_puba.sql
@@ -1,6 +1,19 @@
-SELECT pugiaa.CasellaCompra, pugica.Nom, pugiaa.Article, pugiaa.Nom, pugi.UnitatsAssignades, pugiaa.TipusUnitat,pugiaa.PreuCost, pugiaa.PreuCostTotal
-FROM puignau..ArticleArrivaReserves pugi
-INNER JOIN puignaubcn..ArticleArriva pubaaa on pubaaa.IdLlotjaGirona = pugi.IdLlotja
-INNER JOIN puignau..ArticleArriva pugiaa on pugiaa.Id = pugi.IdLlotja
-LEFT JOIN puignau..CasellesCompres pugica on pugica.Casella = pugiaa.CasellaCompra
-WHERE Client = 6507
+CREATE FUNCTION fpers_reserves_producte_pa_pugi_per_puba(@Data date)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	SELECT pugiaa.CasellaCompra
+		, pugica.Nom as NomCasella
+		, pugiaa.Article
+		, pugiaa.Nom as NomArticle
+		, pugi.UnitatsAssignades
+		, pugiaa.TipusUnitat
+		, pugiaa.PreuCost
+		, pugiaa.PreuCostTotal
+	FROM puignau..ArticleArrivaReserves pugi
+	INNER JOIN puignaubcn..ArticleArriva pubaaa on pubaaa.IdLlotjaGirona = pugi.IdLlotja
+	INNER JOIN puignau..ArticleArriva pugiaa on pugiaa.Id = pugi.IdLlotja
+	LEFT JOIN puignau..CasellesCompres pugica on pugica.Casella = pugiaa.CasellaCompra
+	WHERE Client = 6507 and pugiaa.Data = @Data
+)
