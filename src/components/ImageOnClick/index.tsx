a6f35f025cb5bbe13f56247af46a7a2a623c2feb
estmi-nowtech <esteve.miquel@nowtechsite.com>
2025-08-28 10:49:18 +0200

    update imageonclick component

--- src/components/ImageOnClick/index.tsx
+++ src/components/ImageOnClick/index.tsx
@@ -1,39 +1,31 @@
-  // Import React and the useState hook, used to manage the visibility of the image.
-  import React, { useState } from 'react';
-  // Import clsx library for conditional classes.
-  import clsx from 'clsx';
-  // Import the useBaseUrl function from Docusaurus.
-  import useBaseUrl from '@docusaurus/useBaseUrl';
-  // Import styles from SCSS module.
-  import styles from './styles.module.css';
+import React, { useState, ReactNode } from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
   // Define the type for props passed to the ImageOnClick component.
-  interface ImageOnClickProps {
-    imageUrl: string; // URL of the image
-    altText: string; // Alternative text for the image
-    buttonName: string; // Name of the button to show/hide the image
-  }
-  // Define the ImageOnClick component as a functional component.
-  const ImageOnClick: React.FC<ImageOnClickProps> = ({ imageUrl, altText, buttonName }) => {
-    // State to track whether image should be shown or hidden.
-    const [showImg, setShowImg] = useState(false);
-    // Use the useBaseUrl function to generate the image URL.
-    const generatedImageUrl = useBaseUrl(imageUrl);
+interface ImageOnClickProps {
+  imageUrl: string;   // URL of the image
+  altText: string;    // Alternative text for the image
+  children: ReactNode; // Content inside the tag (button text, icons, etc.)
+}
 
-    return (
-      <span>
-        {/* Button to toggle visibility of the image */}
-        <a onClick={() => setShowImg(!showImg)} className={styles.cursor}>
-          {buttonName}  
-        </a>
-        {/* Conditionally render the image if showImg is true */}
-        {showImg && (
-          <span className={styles.imageonclick}>
-            {/* Image element */}
-            <img src={generatedImageUrl} alt={altText} /> 
-          </span>
-        )}
-      </span>
-    );
-  }
+const ImageOnClick: React.FC<ImageOnClickProps> = ({ imageUrl, altText, children }) => {
+  const [showImg, setShowImg] = useState(false);
+  const generatedImageUrl = useBaseUrl(imageUrl);
+
+  return (
+    <span>
+      {/* Button to toggle visibility of the image */}
+      <a onClick={() => setShowImg(!showImg)} className={styles.cursor}>
+        {children}
+      </a>
+      {/* Conditionally render the image */}
+      {showImg && (
+        <span className={styles.imageonclick}>
+          <img src={generatedImageUrl} alt={altText} />
+        </span>
+      )}
+    </span>
+  );
+};
   {/* Export the ImageOnClick component */}
   export default ImageOnClick;
